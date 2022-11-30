2291ebd7c6c767e7878e21b03ab0f195ac799e63
fu-ning-hsu <86638352+fu-ning-hsu@users.noreply.github.com>
2022-11-30 23:28:30 +0800

    add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+      extend: {
+        gridTemplateRows: {
+          'auto1': 'auto 1fr',
+        },
+        gridTemplateColumns: {
+          'cards': 'repeat(auto-fit, minmax(250px, 1fr))',
+        }
+      },
+    },
+  plugins: [],
+}

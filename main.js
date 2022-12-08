5b2dab1111da9c43113ea2b0dac9c187c741af0c
fu-ning-hsu <86638352+fu-ning-hsu@users.noreply.github.com>
2022-12-08 13:21:59 +0800

    update createEvent

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function mapEventObject(event){
 function createEvent(e, i) {
     return (
         `<article class="bg-white dark:bg-slate-800 shadow-xl shadow-slate-200 dark:shadow-slate-800 rounded-lg">
-            <div class="p-3 shadow bg-indigo-500 text-indigo-50 uppercase grid place-items-center rounded-t-lg">
+            <div class="p-3 shadow bg-emerald-600 text-indigo-50 uppercase grid place-items-center rounded-t-lg">
             <div class="text-sm">${e.start.month}</div>
             <div class="text-3xl font-bold">${e.start.date}</div>
             </div>
@@ -64,23 +64,23 @@ function createEvent(e, i) {
             <div class="grid gap-1">
                 <p class="text-slate-400 text-sm">${e.dateRange}</p>
                 <h2 class="font-bold text-2xl">
-                <a href="${e.link}">${e.name}</a>
-                ${e.location ? `<p class="text-slate-400 text-sm">${e.location}</p>` : ''}
+                <a class="tracking-wide" href="${e.link}">${e.name}</a>
+                ${e.location ? `<p class="text-slate-400 text-sm font-normal mt-1">${e.location}</p>` : ''}
                 </h2>
                 ${e.description ? `
                 <div class="grid gap-1">
-                <button class="text-slate-400 flex gap-1 items-center cursor-pointer" aria-expanded="false" aria-controls="details-${i}" id="btn-${i}">
-                    <p class="pointer-events-none">See details</p>
+                <button class="text-slate-500 flex gap-1 items-center cursor-pointer" aria-expanded="false" aria-controls="details-${i}" id="btn-${i}">
+                    <p class="pointer-events-none tracking-wide mt-1">課程資訊</p>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 pointer-events-none transition-transform" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                     </svg>
                 </button>
                 <div class="grid gap-1 hidden" id="details-${i}" aria-labelledby="btn-${i}">
-                    <p class="text-slate-400">${e.description}</p>
+                    <p class="text-slate-500">${e.description}</p>
                 </div>
                 </div>` : `<div class="h-4"></div>` }
             </div>
-            <a href="${e.link}" class="bg-indigo-500 rounded-md px-4 py-2 text-indigo-50 shadow-2xl shadow-indigo-200 dark:shadow-none text-center font-bold hover:shadow-none ring ring-offset-0 ring-indigo-500 focus:outline-none focus:ring-offset-2">View Event</a>
+            <a target="_blank" href="${e.link}" class="bg-emerald-600 rounded-md px-4 py-2 text-emerald-50 shadow-2xl shadow-emerald-200 dark:shadow-none text-center font-bold hover:shadow-none ring ring-offset-0 ring-emerald-500 focus:outline-none focus:ring-offset-2 tracking-wider">我有興趣</a>
             </div>
         </article>`
     )
